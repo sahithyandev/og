8a12b74be4133cc99c80148aa84ff817c3c424a8
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2022-11-07 07:09:36 +0530

    chore: add main files

--- package/src/index.js
+++ package/src/index.js
@@ -1,1 +1,26 @@
-exports.hi = "hi";
+const satori = require("satori").default;
+const fs = require("fs/promises");
+
+const defaultFont = async () => {
+	const fontBuffer = await fs.readFile(
+		require.resolve("./../assets/Inter-Regular.ttf")
+	);
+
+	return {
+		name: "Inter",
+		data: fontBuffer,
+		weight: 400,
+		style: "normal",
+	};
+};
+
+exports.default = async function (a, options) {
+	const _options = structuredClone(options);
+
+	if (_options.fonts == undefined || _options.fonts.length === 0) {
+		_options.fonts = [await defaultFont()];
+	}
+
+	const svgContent = await satori(a, _options);
+	return svgContent;
+};
--- test/index.js
+++ test/index.js
@@ -1,2 +1,24 @@
-const x = require("vercel-og-node");
-console.log(x.hi);
+const nodeOgImages = require("node-og-images").default;
+const fs = require("fs/promises");
+
+// intended content for testing
+const x = `<div>
+			<h1>Hello JavaScript!</h1>
+
+			<div>
+				<div> 27 February 2021 </div>
+
+				<div>
+					<div>javascript</div>
+					<div>webdev</div>
+				</div>
+			</div>
+		</div>
+`;
+
+nodeOgImages("Hi", {
+	width: 1200,
+	height: 630,
+}).then((svgContent) => {
+	fs.writeFile("./test.svg", svgContent);
+});
