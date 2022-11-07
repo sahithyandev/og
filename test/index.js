ee029582dff62dc8ab0684ca91cbf8fc519ff541
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2022-11-07 07:28:26 +0530

    fix: use a complex structure in test

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ const fs = require("fs/promises");
 
 // intended content for testing
 const x = `<div>
+			<img src="https://sahithyandev.github.io/apple-touch-icon.png" width="180" height="180" />
 			<h1>Hello JavaScript!</h1>
 
 			<div>
@@ -16,9 +17,54 @@ const x = `<div>
 		</div>
 `;
 
-nodeOgImages("Hi", {
-	width: 1200,
-	height: 630,
-}).then((svgContent) => {
+nodeOgImages(
+	{
+		type: "div",
+		props: {
+			style: { display: "flex" },
+			children: [
+				{
+					type: "img",
+					props: {
+						src: "https://sahithyandev.github.io/apple-touch-icon.png",
+						width: "180",
+						height: "180",
+					},
+				},
+				{ type: "h1", props: { children: "Hello JavaScript!" } },
+				{
+					type: "div",
+					props: {
+						style: {
+							display: "flex",
+						},
+						children: [
+							{
+								type: "div",
+								props: {
+									children: "27 February 2021",
+								},
+							},
+							{
+								type: "div",
+								props: {
+									children: [
+										{ type: "div", props: { children: "javascript" } },
+										{ type: "div", props: { children: "webdev" } },
+									],
+									style: { display: "flex" },
+								},
+							},
+						],
+					},
+				},
+			],
+		},
+	},
+	{
+		width: 1200,
+		height: 630,
+	}
+).then((svgContent) => {
 	fs.writeFile("./test.svg", svgContent);
 });
