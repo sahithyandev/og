b246a3235566fad3db264919a93b32513c0ca054
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2022-11-10 12:25:50 +0530

    chore: produce cjs and esm bundles

--- package/tsup.config.ts
+++ package/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: ["src/index.ts"],
 	outDir: "dist",
+	format: ["esm", "cjs"],
 	splitting: false,
 	sourcemap: false,
 	clean: true,
