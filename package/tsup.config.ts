8115b2de082ce6719e25fc7fb70d77a12c328e87
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2022-11-11 11:21:29 +0530

    copy required files from satori under package on build

--- package/tsup.config.ts
+++ package/tsup.config.ts
@@ -1,3 +1,5 @@
+import { join } from "path";
+import { copy } from "fs-extra";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
@@ -8,4 +10,30 @@ export default defineConfig({
 	sourcemap: false,
 	clean: true,
 	dts: true,
+	esbuildPlugins: [
+		{
+			name: "copy-satori",
+			setup(build) {
+				build.onStart(async () => {
+					// copy every file under satori/dist/
+					await copy(join(__dirname, "../deps/satori/"), "./lib/satori/", {
+						overwrite: true,
+						filter: (src) => {
+							return (
+								src.includes("satori/dist") ||
+								src.includes("satori/package.json")
+							);
+						},
+					});
+				});
+				build.onResolve({ filter: /^satori$/ }, async () => {
+					// update the imports
+					return {
+						path: join("../lib/satori/"),
+						external: true,
+					};
+				});
+			},
+		},
+	],
 });
