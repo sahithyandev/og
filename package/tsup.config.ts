a01882d014e536e5bee32affbc32663fd76137e1
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2022-11-11 19:32:10 +0530

    chore: remove unnecessary plugin

--- package/tsup.config.ts
+++ package/tsup.config.ts
@@ -7,17 +7,7 @@ export default defineConfig({
 	format: ["esm", "cjs"],
 	splitting: false,
 	sourcemap: false,
-	clean: true,
+	clean: false,
 	dts: true,
 	noExternal: ["satori"],
-	esbuildPlugins: [
-		{
-			name: "copy-readme",
-			setup(build) {
-				build.onStart(() => {
-					return copy("../README.md", "./README.md", { overwrite: true });
-				});
-			},
-		},
-	],
 });
